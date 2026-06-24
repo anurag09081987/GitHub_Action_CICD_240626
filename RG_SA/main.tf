ab801b613d97a5848678eb08a60c9069ced0adf2
Anurag Chauhan <send2anurag.chauhan@gmail.com>
2026-06-24 23:22:00 +0530

    test1

---
+++ RG_SA/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "this"{
+    name = "anurag"
+    location = "central india"
+}
+resource "azurerm_storage_account" "this"{
+    name = "b18g7new13"
+    resource_group_name = azurerm_resource_group.this.name
+    location = azurerm_resource_group.this.location
+    account_tier = "Standard"
+    account_replication_type = "ZRS"
+}
---
+++ RG_SA/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "4.78.0"
+        }
+    }
+    backend "azurerm" {
+        resource_group_name = pipelinetest1
+        storage_account_name = pipelinetest1
+        container_name = pipelinecontainer
+        key = action1.terraform.tfstate
+    }
+}
+provider "azurerm"{
+    features {}
+    subscription_id = "4e434342-8dc1-4d58-8efd-e7ca00355854"
+}
